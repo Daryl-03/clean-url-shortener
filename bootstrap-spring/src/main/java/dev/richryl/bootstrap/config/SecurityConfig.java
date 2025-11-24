06b1628504e7430e158344cf9f4de1a2b706f90f
Daryl <donamedenou@gmail.com>
2025-11-24 14:42:44 +0100

    feat(api): add health check endpoint

--- bootstrap-spring/src/main/java/dev/richryl/bootstrap/config/SecurityConfig.java
+++ bootstrap-spring/src/main/java/dev/richryl/bootstrap/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
         http
             .authorizeHttpRequests(authorize -> authorize
                     .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                    .requestMatchers(HttpMethod.GET, "/api/health/**").permitAll()
                 .requestMatchers("/api/**").authenticated()
                 .anyRequest().permitAll()
             )
